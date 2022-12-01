017f48bcca4747b84b3b6518d212146f7b9b682c
zmmohibul <18-36427-1@student.aiub.edu>
2022-12-01 07:28:34 +0600

    LeetCode - Array - Minimum Recolors to Get K Consecutive Black Blocks

---
+++ Array/Easy/MinimumRecolorsToGetKConsecutiveBlackBlocks/MinimumRecolorsToGetKConsecutiveBlackBlocks/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Drawing;
+
+namespace MinimumRecolorsToGetKConsecutiveBlackBlocks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+                * You are given a 0 - indexed string blocks of length n, where blocks[i] is either 'W' or 'B', 
+                representing the color of the ith block.The characters 'W' and 'B' denote the colors white and black, respectively.
+
+                * You are also given an integer k, which is the desired number of consecutive black blocks.
+
+                * In one operation, you can recolor a white block such that it becomes a black block.
+
+                * Return the minimum number of operations needed such that there is at least one occurrence of 
+                k consecutive black blocks.
+            */
+
+            /*
+             * 
+             * Input: blocks = "WBBWWBBWBW", k = 7
+             * Output: 3
+             * Explanation:
+                One way to achieve 7 consecutive black blocks is to recolor the 0th, 3rd, and 4th blocks
+                so that blocks = "BBBBBBBWBW". 
+                It can be shown that there is no way to achieve 7 consecutive black blocks in less than 3 operations.
+                Therefore, we return 3.
+            */
+
+            string blocks = "WBBWWBBWBW";
+            int k = 7;
+            Console.WriteLine(Solution.MinimumRecolors(blocks, k));
+        }
+    }
+}
---
+++ Array/Easy/MinimumRecolorsToGetKConsecutiveBlackBlocks/MinimumRecolorsToGetKConsecutiveBlackBlocks/Solution.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinimumRecolorsToGetKConsecutiveBlackBlocks
+{
+    public class Solution
+    {
+        public static int MinimumRecolors(string blocks, int k)
+        {
+            // WBBWWBBWBW
+            //  i      j           {B: 4, W: 3} min = 3
+
+            // Iterate over block k times and build freq map
+            var freq = new Dictionary<char, int>();
+            for (int i = 0; i < k; i++)
+            {
+                freq[blocks[i]] = freq.GetValueOrDefault(blocks[i], 0) + 1;
+            }
+
+            // Calculate how many more B required to get k B's by k - freq[B] 
+            // But first check if 'B' is in freq
+            // Then make min the calculated value
+            int min = 0;
+            if (freq.ContainsKey('B'))
+            {
+                min = k - freq['B'];
+            }
+            else
+            {
+                min = k;
+            }
+
+            // Iterate over blocks with i = 0 and j = k
+            int left = 0;
+            for (int right = k; right < blocks.Length; right++)
+            {
+                // Deduce freq[blocks[i]] and increment freq[blokcs[j]]
+                freq[blocks[left]] -= 1;
+                freq[blocks[right]] = freq.GetValueOrDefault(blocks[right], 0) + 1;
+
+                // Calculate value again and see if it's less than min
+                if (freq.ContainsKey('B'))
+                {
+                    int diff = k - freq['B'];
+                    if (diff < min)
+                    {
+                        min = diff;
+                    }
+                }
+
+                left += 1;
+            }
+
+            // return min
+            return min;
+        }
+    }
+}
