3b3e7698ae213738a258ead1f2f6bf6ec1f0a663
zmmohibul <18-36427-1@student.aiub.edu>
2022-12-02 18:01:06 +0600

    Array - Best Time to Buy and Sell Stock

---
+++ Array/Easy/BestTimeToBuyAndSellStock/BestTimeToBuyAndSellStock/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace BestTimeToBuyAndSellStock
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var prices = new int[] { 7, 1, 5, 3, 6, 4 };
+            Console.WriteLine(Solution.MaxProfit(prices));
+        }
+    }
+}
---
+++ Array/Easy/BestTimeToBuyAndSellStock/BestTimeToBuyAndSellStock/Solution.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BestTimeToBuyAndSellStock
+{
+    public class Solution
+    {
+        public static int MaxProfit(int[] prices)
+        {
+            int max = 0;
+            int i = 0;
+            for (int j = 1; j < prices.Length; j++)
+            {
+                if (prices[j] < prices[i])
+                {
+                    i = j;
+                }
+                else
+                {
+                    max = Math.Max(max, prices[j] - prices[i]);
+                }
+            }
+
+            return max;
+        }
+    }
+}
