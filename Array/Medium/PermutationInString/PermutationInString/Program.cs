5e7167f99aaf18dd652daac56afaace8d2817ac4
zmmohibul <18-36427-1@student.aiub.edu>
2022-12-03 10:56:54 +0600

    Array - Permutation in String

---
+++ Array/Medium/PermutationInString/PermutationInString/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace PermutationInString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var s1 = "ab";
+            var s2 = "eidbaooo";
+            Console.WriteLine(Solution.CheckInclusion(s1, s2));
+        }
+    }
+}
---
+++ Array/Medium/PermutationInString/PermutationInString/Solution.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PermutationInString
+{
+    public class Solution
+    {
+        public static bool CheckInclusion(string s1, string s2)
+        {
+            if (s2.Length < s1.Length)
+            {
+                return false;
+            }
+
+            Dictionary<char, int> s1Freq = new Dictionary<char, int>();
+            for (int i = 0; i < s1.Length; i++)
+            {
+                s1Freq[s1[i]] = s1Freq.GetValueOrDefault(s1[i], 0) + 1;
+            }
+
+            for (int i = 0; i < s1.Length; i++)
+            {
+                if (s1Freq.ContainsKey(s2[i]))
+                {
+                    s1Freq[s2[i]] -= 1;
+                }
+            }
+
+            int k = 0;
+            int j = s1.Length - 1;
+            while (true)
+            {
+                bool hasPermutation = true;
+
+                for (int index = 0; index < s1Freq.Count; index++)
+                {
+                    if (s1Freq.ElementAt(index).Value > 0)
+                    {
+                        hasPermutation = false;
+                        break;
+                    }
+                }
+
+                if (hasPermutation)
+                {
+                    return true;
+                }
+
+                if (s1Freq.ContainsKey(s2[k]))
+                {
+                    s1Freq[s2[k]] += 1;
+                }
+                k += 1;
+
+                j += 1;
+                if (j >= s2.Length)
+                {
+                    break;
+                }
+
+                if (s1Freq.ContainsKey(s2[j]))
+                {
+                    s1Freq[s2[j]] -= 1;
+                }
+            }
+
+            return false;
+
+        }
+    }
+}
