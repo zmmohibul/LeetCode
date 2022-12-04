b997692074c0b3893952d3283ec08bb6c9b2db38
zmmohibul <18-36427-1@student.aiub.edu>
2022-12-04 15:25:32 +0600

    Find Pivot Integer (Easy)

---
+++ Easy/FindPivotInteger/FindPivotInteger/Solution.cs
@@ -0,0 +1,31 @@
+﻿namespace FindPivotInteger;
+
+public class Solution
+{
+    public static int PivotInteger(int n)
+    {
+        // 8
+        // 1 2 3 4 5 6 7 8
+        // {1: 1, 3: 2, 6: 3, 10: 4, 15: 5, 21: 6}
+        int sum = 0;
+        var d = new Dictionary<int, int>();
+        for (int i = 1; i <= n; i++)
+        {
+            sum += i;
+            d[sum] = i;
+        }
+
+        sum = 0;
+        for (int i = n; i > 0; i--)
+        {
+            sum += i;
+            if (d.ContainsKey(sum) && d[sum] == i)
+            {
+                return d[sum];
+            }
+        }
+
+        return -1;
+
+    }
+}
