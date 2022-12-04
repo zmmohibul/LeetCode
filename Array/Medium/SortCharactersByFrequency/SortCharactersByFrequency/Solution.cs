4cb925476673e127c7dd30db991585e2a1aba38a
zmmohibul <18-36427-1@student.aiub.edu>
2022-12-04 12:57:56 +0600

    LeetCode - Sort Characters By Frequency

---
+++ Array/Medium/SortCharactersByFrequency/SortCharactersByFrequency/Solution.cs
@@ -0,0 +1,49 @@
+﻿using System.Text;
+
+namespace SortCharactersByFrequency;
+
+public class Solution
+{
+    public static string FrequencySort(string s)
+    {
+        var sFreq = new Dictionary<char, int>();
+        foreach (var c in s)
+        {
+            sFreq[c] = sFreq.GetValueOrDefault(c, 0) + 1;
+        }
+
+        var pq = new PriorityQueue<char, int>(new IntMaxCompare());
+        foreach (var (c, count) in sFreq)
+        {
+            Console.Write($"{c} - {count}, ");
+            pq.Enqueue(c, count);
+        }
+
+        Console.WriteLine(pq.Count);
+
+        var result = new StringBuilder();
+        var pqC = pq.Count;
+        for (int i = 0; i < pqC; i++)
+        {
+            var element = pq.Dequeue();
+            var elementCount = sFreq[element];
+            Console.Write($"{element} - {elementCount}, ");
+            for (int j = 0; j < elementCount; j++)
+            {
+                result.Append(element);
+            }
+        }
+
+        Console.WriteLine();
+
+        return result.ToString();
+    }
+}
+
+public class IntMaxCompare : IComparer<int>
+{
+    public int Compare(int x, int y)
+    {
+        return y.CompareTo(x);
+    }
+}
