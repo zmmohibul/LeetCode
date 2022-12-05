b3be3ce142ce1bb3c3048bdf20dd8f8b4ca5e31e
zmmohibul <18-36427-1@student.aiub.edu>
2022-12-05 12:26:46 +0600

    Swap Nodes in Pairs - Linked List(Medium)

---
+++ Medium/SwapNodesInPairs/SwapNodesInPairs/Solution.cs
@@ -0,0 +1,43 @@
+﻿namespace SwapNodesInPairs;
+
+public class ListNode {
+    public int val;
+    public ListNode next;
+    public ListNode(int val=0, ListNode next=null) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+public class Solution
+{
+    public ListNode SwapPairs(ListNode head)
+    {
+        if (head == null || head.next == null)
+        {
+            return head;
+        }
+        
+        var sentinel = new ListNode(-1, head);
+        var curr = head;
+        var prev = sentinel;
+        var next = curr.next;
+        while (next != null)
+        {
+            curr.next = next.next;
+            next.next = curr;
+            prev.next = next;
+
+
+            prev = curr;
+            curr = curr.next;
+            if (curr == null)
+            {
+                break;
+            }
+            next = curr.next;
+        }
+
+        return sentinel.next;
+    }
+}
