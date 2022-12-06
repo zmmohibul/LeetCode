5101f75760a00f69e51145c4a6b9314867634cbf
zmmohibul <18-36427-1@student.aiub.edu>
2022-12-06 09:57:19 +0600

    Valid Parentheses (Easy)

---
+++ Easy/ValidParentheses/ValidParentheses/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using ValidParentheses;
+
+Console.WriteLine(Solution.IsValid("()"));
+Console.WriteLine(Solution.IsValid("(){}"));
+Console.WriteLine(Solution.IsValid("(){]"));
+Console.WriteLine(Solution.IsValid("([])({})"));
---
+++ Easy/ValidParentheses/ValidParentheses/Solution.cs
@@ -0,0 +1,46 @@
+﻿namespace ValidParentheses;
+
+public class Solution
+{
+    public static bool IsValid(string s)
+    {
+        var open = new HashSet<char>() { '(', '{', '[' };
+        var close = new HashSet<char>() { ')', '}', ']' };
+
+        var match = new Dictionary<char, char>();
+        match['('] = ')';
+        match['{'] = '}';
+        match['['] = ']';
+
+        var stack = new Stack<char>();
+        for (int i = 0; i < s.Length; i++)
+        {
+            var ch = s[i];
+            if (open.Contains(ch))
+            {
+                stack.Push(ch);
+            }
+            else
+            {
+                if (stack.Count == 0)
+                {
+                    return false;
+                }
+                var last = stack.Pop();
+                var lastCl = match[last];
+
+                if (ch != lastCl)
+                {
+                    return false;
+                }
+            }
+        }
+
+        if (stack.Count > 0)
+        {
+            return false;
+        }
+
+        return true;
+    }
+}
