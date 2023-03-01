89680d7de538ab3f87397d378dc2a5108f410b69
zmmohibul <18-36427-1@student.aiub.edu>
2023-03-01 10:09:07 +0600

    Minimum Absolute Difference Optimized

--- Easy/MinimumAbsoluteDifference/MinimumAbsoluteDifference/Solution.cs
+++ Easy/MinimumAbsoluteDifference/MinimumAbsoluteDifference/Solution.cs
@@ -4,12 +4,14 @@ public class Solution {
     public IList<IList<int>> MinimumAbsDifference(int[] arr) {
         Array.Sort(arr);
         int minDiff = int.MaxValue;
+        var diffListMap = new Dictionary<int, IList<IList<int>>>();
         // -46 -24 -5 2 34 60
         for (int i = 1; i < arr.Length; i++)
         {
             var n2 = arr[i];
             var n1 = arr[i - 1];
             int diff = 0;
+            
             if (n1 <= 0 && n2 <= 0)
             {
                 diff = Math.Abs(Math.Abs(n2) - Math.Abs(n1));
@@ -28,34 +30,15 @@ public class Solution {
             {
                 minDiff = diff;
             }
+
+            if (!diffListMap.ContainsKey(diff))
+            {
+                diffListMap[diff] = new List<IList<int>>();
+            }
+            
+            diffListMap[diff].Add(new List<int>() { Math.Min(arr[i], arr[i - 1]), Math.Max(arr[i], arr[i - 1]) });
         }
 
-        var result = new List<IList<int>>();
-        for (int i = 1; i < arr.Length; i++)
-        {
-            var n2 = arr[i];
-            var n1 = arr[i - 1];
-            int diff = 0;
-            if (n1 <= 0 && n2 <= 0)
-            {
-                diff = Math.Abs(Math.Abs(n2) - Math.Abs(n1));
-            }
-            else if (n1 >= 0 && n2 >= 0)
-            {
-                diff = n2 - n1;
-            }
-            else if (n1 <= 0 && n2 > 0)
-            {
-                n1 = Math.Abs(n1);
-                diff = n1 + n2;
-            }
-
-            if (diff == minDiff)
-            {
-                result.Add(new List<int>() { Math.Min(arr[i], arr[i - 1]), Math.Max(arr[i], arr[i - 1]) });
-            }
-        }
-
-        return result;
+        return diffListMap[minDiff];
     }
 }
