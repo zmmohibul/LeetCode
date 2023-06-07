55ae25f9d670860476d5f1aae25bf50b6d80aa47
zmmohibul <18-36427-1@student.aiub.edu>
2023-06-07 08:46:51 +0600

    Find Root of N-Ary Tree - Space O(1), Time O(n)

--- Medium/FindRootOfNAryTree/Solution.cs
+++ Medium/FindRootOfNAryTree/Solution.cs
@@ -4,36 +4,27 @@ public class Solution
 {
     public Node FindRoot(List<Node> tree)
     {
-        var node = tree[0];
-        var parent = node;
-        while (true)
-        {
-            parent = FindParent(node, tree);
-            if (parent == null)
-            {
-                return node;
-            }
-            else
-            {
-                node = parent;
-            }
-        }
-
-        
-    }
-
-    public Node FindParent(Node n, List<Node> tree)
-    {
+        var childSum = 0;
         for (int i = 0; i < tree.Count; i++)
         {
-            var node = tree[i];
-            for (int j = 0; j < node.children.Count; j++)
+            for (int j = 0; j < tree[i].children.Count; j++)
             {
-                var child = node.children[j];
-                if (child.val == n.val)
-                {
-                    return node;
-                }
+                childSum += tree[i].children[j].val;
+            }
+        }
+        
+        var nodeSum = 0;
+        for (int i = 0; i < tree.Count; i++)
+        {
+            nodeSum += tree[i].val;
+        }
+
+        var diff = nodeSum - childSum;
+        for (int i = 0; i < tree.Count; i++)
+        {
+            if (tree[i].val == diff)
+            {
+                return tree[i];
             }
         }
 
