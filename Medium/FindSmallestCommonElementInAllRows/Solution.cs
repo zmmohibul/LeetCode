75822cab0274e3127187bc3995935b75a7e172da
zmmohibul <18-36427-1@student.aiub.edu>
2023-06-09 21:33:58 +0600

    Find Smallest Common Element in All Rows

--- Medium/FindSmallestCommonElementInAllRows/Solution.cs
+++ Medium/FindSmallestCommonElementInAllRows/Solution.cs
@@ -2,37 +2,55 @@ namespace FindSmallestCommonElementInAllRows;
 
 public class Solution 
 {
-    public int SmallestCommonElement(int[][] mat) 
+    public int SmallestCommonElement(int[][] mat)
     {
-        var elementFreq = new Dictionary<int, int>();
-        for (int i = 0; i < mat.Length; i++)
+        var firstRow = mat[0];
+        for (int i = 0; i < firstRow.Length; i++)
         {
-            var row = mat[i];
-            var elementsInRow = new HashSet<int>();
-            
-            for (int j = 0; j < row.Length; j++)
+            var element = firstRow[i];
+            var common = true;
+            for (int j = 0; j < mat.Length; j++)
             {
-                if (!elementsInRow.Contains(row[j]))
+                if (!BinarySearch(mat[j], element))
                 {
-                    elementFreq[row[j]] = elementFreq.GetValueOrDefault(row[j], 0) + 1;
+                    common = false;
+                    break;
                 }
-
-                elementsInRow.Add(row[j]);
             }
-        }
 
-        for (int i = 0; i < elementFreq.Count; i++)
-        {
-            var item = elementFreq.ElementAt(i);
-            var element = item.Key;
-            var count = item.Value;
-
-            if (count == mat.Length)
+            if (common)
             {
                 return element;
             }
         }
 
         return -1;
+    }
+
+    public bool BinarySearch(int[] arr, int target)
+    {
+        var left = 0;
+        var right = arr.Length - 1;
+        
+        while (left <= right)
+        {
+            var mid = left + (right - left) / 2;
+            if (arr[mid] == target)
+            {
+                return true;
+            }
+            
+            if (arr[mid] < target)
+            {
+                left = mid + 1;
+            }
+            else
+            {
+                right = mid - 1;
+            }
+        }
+
+        return false;
+        
     }
 }
