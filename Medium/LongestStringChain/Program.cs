4251caad26fd922daca7ecb894f1168ff7f615bb
zmmohibul <18-36427-1@student.aiub.edu>
2023-09-24 14:39:55 +0600

    Longest String Chain (Bit of Optimization)

--- Medium/LongestStringChain/Program.cs
+++ Medium/LongestStringChain/Program.cs
@@ -1,5 +1,9 @@
 ﻿using LongestStringChain;
 
-var words = new string[] {"a","b","ba","bca","bda","bdca"};
 var sl = new Solution();
+
+var words = new string[] {"a","b","ba","bca","bda","bdca"};
+Console.WriteLine(sl.LongestStrChain(words));
+
+words = new string[] {"a","b","ba","bca","bda","bdca","abcdefghijkl","abcdefghijk","abcdefghij","abcdefghi","abcdefgh","abcdefg","abcdef","abcde"};
 Console.WriteLine(sl.LongestStrChain(words));
--- Medium/LongestStringChain/Solution.cs
+++ Medium/LongestStringChain/Solution.cs
@@ -2,47 +2,63 @@ namespace LongestStringChain;
 
 public class Solution 
 {
-    Dictionary<string, int> wordChains = new Dictionary<string, int>();
+    private Dictionary<string, int> wordChains = new Dictionary<string, int>();
 
-    public int LongestStrChain(string[] words) 
+    public int LongestStrChain(string[] words)
     {
+        var wordsGroupedByLength = new Dictionary<int, HashSet<string>>();
+        foreach (var word in words)
+        {
+            var length = word.Length;
+            if (!wordsGroupedByLength.ContainsKey(length))
+            {
+                wordsGroupedByLength[length] = new HashSet<string>();
+            }
+
+            wordsGroupedByLength[length].Add(word);
+        }
+        
         var chains = new HashSet<int>();
         foreach (var word in words)
         {
-            chains.Add(LongestStrChainHelper(words, word));
+            chains.Add(LongestStrChainHelper(wordsGroupedByLength, word));
         }
 
         return chains.Max();
     }
-
-    private int LongestStrChainHelper(string[] words, string currWord) 
+    
+    private int LongestStrChainHelper(Dictionary<int, HashSet<string>> wordGroups, string currWord) 
     {
         if (wordChains.ContainsKey(currWord))
         {
             return wordChains[currWord];
         }
-        else if (currWord.Length == 1)
+        
+        if (currWord.Length == 1)
         {
+            wordChains[currWord] = 1;
             return 1;
         }
-        else
+        
+        var chains = new HashSet<int>(){1};
+        if (wordGroups.ContainsKey(currWord.Length - 1))
         {
-            var chains = new HashSet<int>(){1};
-            foreach (var word in words)
+            var wordsToExplore = wordGroups[currWord.Length - 1];
+            foreach (var word in wordsToExplore)
             {
                 if (word.Length == currWord.Length - 1)
                 {
                     if (IsPredecessor(currWord, word))
                     {
-                        chains.Add(1 + LongestStrChainHelper(words, word));
+                        chains.Add(1 + LongestStrChainHelper(wordGroups, word));
                     }
                 }
             }
-
-            var max = chains.Max();
-            wordChains[currWord] = max;
-            return max;
         }
+
+        var max = chains.Max();
+        wordChains[currWord] = max;
+        return max;
     }
 
     private bool IsPredecessor(string big, string small)
