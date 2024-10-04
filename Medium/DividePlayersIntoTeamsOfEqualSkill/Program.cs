1a52906e28a79595d516eb09d4bef377fcfa7662
zmmohibul <18-36427-1@student.aiub.edu>
2024-10-04 11:22:33 +0600

    Divide Players Into Teams of Equal Skill

---
+++ Medium/DividePlayersIntoTeamsOfEqualSkill/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+
+long DividePlayers(int[] skill) {
+    Array.Sort(skill);
+    var teamSkill = skill[0] + skill[^1];
+    var left = 1;
+    long answer = skill[0] * skill[^1];
+    for (var right = skill.Length - 2; left < right; right--)
+    {
+        if (skill[left] + skill[right] != teamSkill) return -1;
+        answer += (skill[left] * skill[right]);
+        left++;
+    }
+
+    return answer;
+}
